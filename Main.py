3e15636f79fac0dbad60885ec76496567d9d5d7d
Владимир Елезаров <wavilla23@gmail.com>
2021-04-20 10:45:34 +0300

    Fourth commit. I don't remember actually what I did. Quite a mistake. And it's been a while since last commit.

--- Main.py
+++ Main.py
@@ -9,63 +9,77 @@ def start_game():
 
 
 def first_settings_set():
-    sides = sides_choosing()
-    order = who_is_first_choosing(sides)
-    return order
+    order = who_is_first_choosing()
+    order_with_symbols = symbols_choosing(order)
+    return order_with_symbols
 
 
-def sides_choosing():
-    pl1 = ""
-    pl2 = ""
-    i = False
-    while not i:
-        pl1 = input("First player - which symbol do you choose? X or O: ").upper()
+def symbols_choosing(players_order):
+    players_symbols = dict()
+    key = False
+    while not key:
+        pl1 = input(f"{players_order[0]} - which symbol do you choose? X or O: ").upper()
         if pl1 == 'X':
             pl2 = 'O'
-            print(f"First player will be - {pl1}, and Second - {pl2}")
-            i = True
+            print(f"{players_order[0]} will be - {pl1}, and {players_order[1]} - {pl2}")
+            players_symbols = dict(first={players_order[0]: pl1}, second={players_order[1]: pl2})
+            key = True
         elif pl1 == 'O':
             pl2 = 'X'
-            print(f"First player will be - {pl1}, and Second - {pl2}")
-            i = True
+            print(f"{players_order[0]} will be - {pl1}, and {players_order[1]} - {pl2}")
+            players_symbols = dict(first={players_order[0]: pl1}, second={players_order[1]: pl2})
+            key = True
         else:
             print("Wrong symbol, try again")
-    players_symbols = dict(pl1=f"{pl1}", pl2=f"{pl2}")
     return players_symbols
 
 
-def who_is_first_choosing(sides):
-    players_pos = dict()
-    rnd = random.Random()
-    ii = False
-    i = False
-    while not ii:
-        while not i:
-            pl1_input = input("First player, please enter number form 0 to 100: ")
-            if pl1_input.isdigit() and 0 < int(pl1_input) <= 100:
-                pl1_input = int(pl1_input)
-            else:
-                print("Try again (not a number or not in a range)")
-                continue
-            pl2_input = input("Second player, please enter number form 0 to 100: ")
-            if pl2_input.isdigit() and 0 < int(pl2_input) <= 100:
-                pl2_input = int(pl2_input)
-                ii = True
-            else:
-                print("Try again (not a number or not in a range)")
+def nickname_input():
+    user_input = ""
+    key = False
+    while not key:
+        user_input = input("Please input your nickname (any string without spaces and no 50 symbols longer): ")
+        if " " in user_input and len(user_input) > 50 or " " in user_input or len(user_input) > 50:
+            print("No-no-no! Try again!")
+        else:
+            key = True
+    return user_input
 
-            number = rnd.randrange(101)
-            print(f"First player - {pl1_input}, Second player - {pl2_input}, Random number - {number}")
-            if abs(number - pl1_input) < abs(number - pl2_input):
-                players_pos = {'first move': sides['pl1'], 'second move': sides['pl2']}
-                print("First player closer to random number - first move")
-                i = True
-            elif abs(number - pl1_input) == abs(number - pl2_input):
-                continue
-            else:
-                players_pos = {'first move': sides['pl2'], 'second move': sides['pl1']}
-                print("Second player closer to random number - first move")
-                i = True
+
+def input_for_choosing_order(nickname):
+    user_input = ""
+    key = False
+    while not key:
+        user_input = input(f"{nickname}, please enter number form 0 to 100: ")
+        if user_input.isdigit() and 0 < int(user_input) <= 100:
+            user_input = int(user_input)
+            key = True
+        else:
+            print("Try again (not a number or not in a range)")
+    return user_input
+
+
+def who_is_first_choosing():
+    players_pos = []
+    rnd = random.Random()
+    first_player_name = nickname_input()
+    second_player_name = nickname_input()
+    key = False
+    while not key:
+        pl1_input = input_for_choosing_order(first_player_name)
+        pl2_input = input_for_choosing_order(second_player_name)
+        number = rnd.randrange(101)
+        print(f"{first_player_name} - {pl1_input}, {second_player_name} - {pl2_input}, Random number - {number}")
+        if abs(number - pl1_input) < abs(number - pl2_input):
+            print(f"{first_player_name} closer to random number - first move")
+            players_pos = [first_player_name, second_player_name]
+            key = True
+        elif abs(number - pl1_input) == abs(number - pl2_input):
+            print("It's a tie. Let's go again")
+        else:
+            print(f"{second_player_name} closer to random number - first move")
+            players_pos = [second_player_name, first_player_name]
+            key = True
     return players_pos
 
 
@@ -93,28 +107,52 @@ def generate_empty_field():
     return empty_field
 
 
-def order_making(field, order):
-    total_moves = len(field) * len(field[0])
+def mover_line(mover, mover_symbol, field):
+    print(f"now {mover} is making move")
+    making_a_move(field, mover_symbol)
+    if game_status_checking(field, mover_symbol):
+        show_field(field)
+        return True
+    show_field(field)
+    return False
+
+
+def order_making(field, order: dict):
+    max_possible_moves = len(field) * len(field[0])
+    players = list(order.values())
+    first_mover = str(list(players[0].keys())[0])
+    first_mover_symbol = str(list(players[0].values())[0])
+    second_mover = str(list(players[1].keys())[0])
+    second_mover_symbol = str(list(players[1].values())[0])
     i = 1
-    while i <= total_moves:
+    while i <= max_possible_moves:
         if i % 2 != 0:
-            print(f"now {order['first move']} is making move")
-            making_a_move(field, order['first move'])
-            if game_status_checking(field, order['first move']):
+            if mover_line(first_mover, first_mover_symbol, field):
                 break
-            show_field(field)
             i += 1
         else:
-            print(f"now {order['second move']} is making move")
-            making_a_move(field, order['second move'])
-            if game_status_checking(field, order['second move']):
+            if mover_line(second_mover, second_mover_symbol, field):
                 break
-            show_field(field)
             i += 1
+
+def making_a_move(field, mover):
+    changed_field = field
+    user_input = list(map(int, input("Enter coords via space: ").split()))
+    for item in user_input:
+        if item > len(changed_field[0]) or item < 1:
+            print(f"Too big number(or negative). Field is only this long {len(changed_field[0])}")
+
+    if changed_field[user_input[0] - 1][user_input[1] - 1] == 0:
+        if mover == 'X':
+            changed_field[user_input[0] - 1][user_input[1] - 1] = "X"
+        else:
+            changed_field[user_input[0] - 1][user_input[1] - 1] = "O"
+    elif changed_field[user_input[0] - 1][user_input[1] - 1] != 0:
+        print("There is already something.")
+    return changed_field
 
 
 def game_status_checking(field, mover):
-
     if diagonal_win_checking(field, mover, lambda g: g):
         return True
     elif diagonal_win_checking(field, mover, lambda g: (len(field) - 1) - g):
@@ -164,21 +202,5 @@ def diagonal_win_checking(field, mover, rule):
     return False
 
 
-def making_a_move(field, mover):
-    changed_field = field
-    user_input = list(map(int, input("Enter coords via space: ").split()))
-    for item in user_input:
-        if item > len(changed_field[0]) or item < 1:
-            print(f"Too big number(or negative). Field is only this long {len(changed_field[0])}")
-
-    if changed_field[user_input[0] - 1][user_input[1] - 1] == 0:
-        if mover == 'X':
-            changed_field[user_input[0] - 1][user_input[1] - 1] = "X"
-        else:
-            changed_field[user_input[0] - 1][user_input[1] - 1] = "O"
-    elif changed_field[user_input[0] - 1][user_input[1] - 1] != 0:
-        print("There is already something.")
-    return changed_field
-
-
-start_game()
+if __name__ == '__main__':
+    start_game()
